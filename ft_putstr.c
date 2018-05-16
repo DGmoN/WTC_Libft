1fcc1ef28a4d33fbc6203c27bbfa2760a1893c0c
William GOURLEY <wgourley@e4r1p1.wethinkcode.co.za>
2018-05-16 14:24:32 +0200

    o_o

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: wgourley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 07:44:35 by wgourley          #+#    #+#             */
-/*   Updated: 2018/05/15 07:44:38 by wgourley         ###   ########.fr       */
+/*   Updated: 2018/05/16 14:24:20 by wgourley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putstr(char *str)
 	int index;
 
 	index = 0;
-	while (str[index])
+	while (str[index] != '\0')
 	{
 		ft_putchar(str[index++]);
 	}
